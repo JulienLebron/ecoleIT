6d908e054e4ba8717a83f84df67e5f39e38a23c2
Julien Lebron <lebron-pro@outlook.fr>
2024-10-21 10:44:58 +0200

    feat: système d'inscription

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,6 +20,10 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('image')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             // nous allons commenter ce champ car nous ne voulons pas que l'utilisateur entre lui m$eme une date de création. Cela sera fait lors du traitement du formulaire, avant l'insertion en bdd de l'article
             // ->add('createdAt')
         ;
